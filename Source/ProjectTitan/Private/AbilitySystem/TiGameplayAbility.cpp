7fc17b98263e0dad0ed88eb1c5f980924b4b1b87
solidmike149 <michele.terrani@protonmail.com>
2022-06-27 15:31:06 +0200

    Added Boss Combo & Shoot Stamina Drain

--- Source/ProjectTitan/Private/AbilitySystem/TiGameplayAbility.cpp
+++ Source/ProjectTitan/Private/AbilitySystem/TiGameplayAbility.cpp
@@ -1,11 +1,52 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "AbilitySystem/TiGameplayAbility.h"
+
+#include "AbilitySystem/TiAbilitySystemComponent.h"
+
 
 UTiGameplayAbility::UTiGameplayAbility()
 {
 	ReplicationPolicy = EGameplayAbilityReplicationPolicy::ReplicateNo;
 
 	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+}
+
+void UTiGameplayAbility::TryActivateAbilityOnSpawn(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) const
+{
+
+	// Try to activate if activation policy is on spawn.
+	if (ActorInfo && !Spec.IsActive())
+	{
+		UAbilitySystemComponent* ASC = ActorInfo->AbilitySystemComponent.Get();
+		const AActor* AvatarActor = ActorInfo->AvatarActor.Get();
+
+		// If avatar actor is torn off or about to die, don't try to activate until we get the new one.
+		if (ASC && AvatarActor && (AvatarActor->GetLifeSpan() <= 0.0f))
+		{
+			
+			ASC->TryActivateAbility(Spec.Handle);
+		}
+	}
+}
+
+void UTiGameplayAbility::OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+	Super::OnGiveAbility(ActorInfo, Spec);
+
+	K2_OnAbilityAdded();
+
+	//TryActivateAbilityOnSpawn(ActorInfo, Spec);
+}
+
+void UTiGameplayAbility::OnRemoveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+	K2_OnAbilityRemoved();
+
+	Super::OnRemoveAbility(ActorInfo, Spec);
+}
+
+void UTiGameplayAbility::OnPawnAvatarSet()
+{
+	K2_OnPawnAvatarSet();
 }
--- Source/ProjectTitan/Public/AbilitySystem/TiGameplayAbility.h
+++ Source/ProjectTitan/Public/AbilitySystem/TiGameplayAbility.h
@@ -8,12 +8,41 @@
 #include "TiGameplayAbility.generated.h"
 
 /**
- * 
+ * UTiGameplayAbility
+ *
+ *	The base gameplay ability class used by this project.
  */
-UCLASS(Abstract)
+UCLASS(Abstract, HideCategories = Input, Meta = (ShortTooltip = "The base gameplay ability class used by this project."))
 class PROJECTTITAN_API UTiGameplayAbility : public UGameplayAbility
 {
 	GENERATED_BODY()
+
+
+public:
+
+	void TryActivateAbilityOnSpawn(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) const;
+	
+protected:
+
+	//~UGameplayAbility interface
+	virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+	virtual void OnRemoveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+	//~End of UGameplayAbility interface
+
+	virtual void OnPawnAvatarSet();
+
+	/** Called when this ability is granted to the ability system component. */
+	UFUNCTION(BlueprintImplementableEvent, Category = Ability, DisplayName = "OnAbilityAdded")
+	void K2_OnAbilityAdded();
+
+	/** Called when this ability is removed from the ability system component. */
+	UFUNCTION(BlueprintImplementableEvent, Category = Ability, DisplayName = "OnAbilityRemoved")
+	void K2_OnAbilityRemoved();
+
+	/** Called when the ability system is initialized with a pawn avatar. */
+	UFUNCTION(BlueprintImplementableEvent, Category = Ability, DisplayName = "OnPawnAvatarSet")
+	void K2_OnPawnAvatarSet();
+	
 
 public:
 	
